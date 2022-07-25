a83954715ee46463ab9a75df3898f566584b595e
Yoshi Tashiro <tashiro@quartile.co>
2022-07-25 01:40:44 +0000

    [2540][IMP] product_prep_state: switch category to purchase

--- product_prep_state/__manifest__.py
+++ product_prep_state/__manifest__.py
@@ -3,14 +3,15 @@
 {
     "name": "Product Prep State",
     "version": "15.0.1.0.0",
-    "category": "Stock",
+    "category": "Inventory/Purchase",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "depends": ["stock"],
+    "depends": ["purchase"],
     "license": "LGPL-3",
     "data": [
         "security/ir.model.access.csv",
         "views/product_prep_state_views.xml",
+        "views/product_product_views.xml",
         "views/product_template_views.xml",
     ],
     "installable": True,
