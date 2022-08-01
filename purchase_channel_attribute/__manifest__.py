1805af4ec4798cf62bac512994aaaab9b1b11461
kanda999 <kanda@quartile.co>
2022-08-01 01:46:38 +0000

    change module name

--- purchase_channel_attribute/__manifest__.py
+++ purchase_channel_attribute/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Purchase Add Info",
+    "name": "Purchase Channel Attribute",
     "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Purchase",
     "license": "LGPL-3",
-    "depends": ["hr", "purchase"],
+    "depends": ["purchase"],
     "data": [
         "security/ir.model.access.csv",
         "data/menuitem_data.xml",
