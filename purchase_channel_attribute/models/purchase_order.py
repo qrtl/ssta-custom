7faea538aed07ce866dc6d9e9b0c9b0638ea5950
kanda999 <kanda@quartile.co>
2022-08-01 02:03:16 +0000

    pre-commit run

--- purchase_channel_attribute/models/purchase_order.py
+++ purchase_channel_attribute/models/purchase_order.py
@@ -1,12 +1,15 @@
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     request_medium_id = fields.Many2one("request.medium", "Request Medium")
-    purchase_category_id = fields.Many2one("purchase.category", "Purchase Category",)
+    purchase_category_id = fields.Many2one(
+        "purchase.category",
+        "Purchase Category",
+    )
     shop_id = fields.Many2one("stock.warehouse", "Shop")
