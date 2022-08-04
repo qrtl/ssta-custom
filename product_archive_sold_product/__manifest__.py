dc82c117b250e341497aa7b25f018e031bfe0562
kanda999 <101688144+kanda999@users.noreply.github.com>
2022-08-04 10:22:09 +0900

    st1 [2495][ADD] product_archive_sold_product (#13)
    This module provides the function to archive "sold" products with cron.

---
+++ product_archive_sold_product/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product Archive Sold Product",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "depends": ["sale_stock"],
+    "data": ["data/cron_data.xml"],
+    "installable": True,
+}
---
+++ product_archive_sold_product/models/product_product.py
@@ -0,0 +1,38 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.model
+    def _process_product_archive(self):
+        self.env.cr.execute(
+            """
+            SELECT
+                sol.product_id
+            FROM
+                sale_order_line sol
+            JOIN
+                product_product pp ON sol.product_id = pp.id
+            JOIN
+                product_template pt ON pp.product_tmpl_id = pt.id
+            WHERE
+                sol.state in ('sale', 'done')
+                AND sol.qty_delivered > 0
+                AND pp.active = True
+                AND pt.type in ('product','consu')
+            GROUP BY
+                sol.product_id
+        """
+        )
+
+        prod_ids = [r[0] for r in self.env.cr.fetchall()]
+
+        for prod in self.browse(prod_ids):
+            # if product has reordering rules then deactivate first
+            if prod.orderpoint_ids:
+                prod.orderpoint_ids.write({"active": False})
+            prod.product_tmpl_id.write({"active": False})
