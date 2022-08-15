211346000a41d38617479546be6a1b5952b35b4d
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-08-15 17:36:08 +0900

    [2361][ADD] web_hide_user_menu_item (#16)
    [2361](https://www.quartile.co/web?debug=1#id=2631&action=771&model=project.task&view_type=form&menu_id=505)

---
+++ web_hide_user_menu_item/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Web Hide User Menu Item",
+    "category": "Hidden",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_qweb": [
+            "web_hide_user_menu_item/static/src/webclient/user_menu/user_menu.xml",
+        ],
+    },
+    "installable": True,
+}
