2c36216dd9dc19fcc3b8a5b4753704e1853cea4c
Aung Ko Ko Lin <aungkokolin@Aungs-MacBook-Pro.local>
2022-08-19 11:04:08 +0630

    [ADD]web_portal_page

---
+++ web_portal_link/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "My Page",
+    "summary": "When cklick the app, system will open my page in new tab.",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Portal",
+    "version": "15.0.1.0.0",
+    "depends": ["portal"],
+    "data": [
+        "views/views.xml",
+    ],
+    "application": True,
+    "license": "AGPL-3",
+    "installable": True,
+}
