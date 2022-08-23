09991d44cb3783a88e26b28452c2c7d1d3828659
AungKoKoLin <45355704+AungKoKoLin1997@users.noreply.github.com>
2022-08-23 12:53:59 +0630

    st1 [2633][ADD] web_portal_link (#21)
    Add module for web_portal_link.

---
+++ web_portal_link/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Web Portal Link",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Portal",
+    "version": "15.0.1.0.0",
+    "depends": ["portal"],
+    "data": [
+        "views/views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
