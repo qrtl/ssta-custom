7611d3855f157e8b1a522412b881da8507d45264
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-07 08:46:46 +0630

    [MIG] product_yahoo_auction_sst: Migration to 15.0

--- product_yahoo_auction_sst/models/yahoo_product_state.py
+++ product_yahoo_auction_sst/models/yahoo_product_state.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -7,7 +7,4 @@ from odoo import fields, models
 class YahooProductState(models.Model):
     _name = "yahoo.product.state"
 
-    name = fields.Char(
-        string="Status",
-        require=True,
-    )
+    name = fields.Char(string="Status", require=True)
