994683425b3d27295d2e3c00725e97551fd44315
AungKoKoLin <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-01-04 11:57:41 +0630

    [3071][ADD] sale_order_line_case_number (#45)
    [ADD] sale_order_line_case_number

---
+++ sale_order_line_case_number/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Line Case Number",
+    "category": "Sale",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["product_auction", "sale"],
+    "installable": True,
+}
