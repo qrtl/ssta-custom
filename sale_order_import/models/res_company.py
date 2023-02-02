e8785d70907a0020ae4715509608520c76d514b6
Aungkokolin1997 <lin@quartile.co>
2023-02-02 19:56:31 +0630

    fix selection field

--- sale_order_import/models/res_company.py
+++ sale_order_import/models/res_company.py
@@ -7,11 +7,11 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    def _picking_policy_selection(self):
+        return self.env["sale.order"]._fields["picking_policy"].selection
+
     picking_policy = fields.Selection(
-        [
-            ("direct", "Deliver each product when available"),
-            ("one", "Deliver all products at once"),
-        ],
+        _picking_policy_selection,
         string="Shipping Policy",
     )
     customer_invoice_journal_id = fields.Many2one(
