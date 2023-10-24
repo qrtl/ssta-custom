dbcc4c639da2d517edeab9b6617a3f176c4e6c7f
Aungkokolin1997 <lin@quartile.co>
2023-10-24 09:33:01 +0630

    [IMP] product_delivery_destination: black, isort, prettier

--- product_delivery_destination/models/delivery_city.py
+++ product_delivery_destination/models/delivery_city.py
@@ -5,8 +5,10 @@ from odoo import fields, models
 
 
 class DeliveryCity(models.Model):
-    _name = 'delivery.city'
-    _description = 'Delivery City'
+    _name = "delivery.city"
+    _description = "Delivery City"
 
-    name = fields.Char(string='City Name', required=True)
-    state_id = fields.Many2one("res.country.state", required=True, domain="[('is_deliverable','=', True)]")
+    name = fields.Char(string="City Name", required=True)
+    state_id = fields.Many2one(
+        "res.country.state", required=True, domain="[('is_deliverable','=', True)]"
+    )
