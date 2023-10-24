d4bcfee6696a7b8af23c1ae9ef68088a0a866ef8
Aungkokolin1997 <lin@quartile.co>
2023-10-24 16:29:24 +0630

    [MIG] product_group_product_manager: Migration to 15.0

--- product_group_product_manager/__manifest__.py
+++ product_group_product_manager/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product Group Product Manager",
-    "version": "11.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Product",
