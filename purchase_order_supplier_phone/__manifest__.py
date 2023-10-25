5e799094c8783fae8da70b699a6cdd447972dbb8
Aungkokolin1997 <lin@quartile.co>
2023-10-25 16:36:45 +0630

    [IMP] purchase_order_supplier_phone: add functionals from sst

--- purchase_order_supplier_phone/__manifest__.py
+++ purchase_order_supplier_phone/__manifest__.py
@@ -1,13 +1,15 @@
 # Copyright 2022 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Add supplier phone and mobile in purchase",
     "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Purchase",
-    "license": "LGPL-3",
-    "depends": ["purchase"],
+    "license": "AGPL-3",
+    "depends": [
+        "purchase_ext_sst",  # is_default_partner()
+    ],
     "data": [
         "views/purchase_order_views.xml",
     ],
