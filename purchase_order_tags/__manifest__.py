88a20498babd7db00fc300e79f0e1ccee59a5e28
Aungkokolin1997 <lin@quartile.co>
2023-10-25 14:49:16 +0630

    [MIG] purchase_order_tags: Migration to 15.0

--- purchase_order_tags/__manifest__.py
+++ purchase_order_tags/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Purchase Order Tags",
-    "version": "11.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "LGPL-3",
