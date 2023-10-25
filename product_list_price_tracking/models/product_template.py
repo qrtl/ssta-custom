0142cd6fc68108787ab0ac13b0dc9dacaccc7e2e
Aungkokolin1997 <lin@quartile.co>
2023-10-25 08:59:00 +0630

    [MIG] product_list_price_tracking: Migration to 15.0

--- product_list_price_tracking/models/product_template.py
+++ product_list_price_tracking/models/product_template.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    list_price = fields.Float(track_visibility="onchange")
+    list_price = fields.Float(tracking=True)
