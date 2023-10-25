461e8d8e84c2ed360364dc1980135cce1b3195c0
Aungkokolin1997 <lin@quartile.co>
2023-10-25 09:59:15 +0630

    [MIG] product_shelf_info: Migration to 15.0

--- product_shelf_info/__manifest__.py
+++ product_shelf_info/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Product Shelf Information",
-    "version": "11.0.1.1.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Product",
--- product_shelf_info/readme/DESCRIPTION.rst
+++
@@ -1,4 +0,0 @@
-This module does the following:
-
-* Creates shelf_id field to product template.
-* Adds shelf_id to delivery slip report, product list view and product form view
