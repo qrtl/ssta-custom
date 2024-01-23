5984a57951c737900d211b83f5553460da028fc1
Aungkokolin1997 <lin@quartile.co>
2024-01-23 10:17:35 +0630

    [MIG] stock_quant_list_view: Migration to 15.0

--- stock_quant_list_view/__manifest__.py
+++ stock_quant_list_view/__manifest__.py
@@ -2,12 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Quant List View",
-    "version": "11.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Stock",
     "license": "AGPL-3",
-    "depends": ["stock", "stock_quant_sales_channel", "product_yahoo_auction_sst"],
+    "depends": [
+        "stock_sales_channel",  # sales_channel_id
+        "product_yahoo_auction_sst",  # yahoo_product_state_id
+    ],
     "data": ["views/stock_quant_views.xml"],
     "installable": True,
 }
---
+++ stock_quant_list_view/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module adds a new menu for stock quant with some specific fields.
