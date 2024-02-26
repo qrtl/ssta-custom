e564d305d2bf7ada576b193f0b0de10b1704555c
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-02-26 11:35:02 +0630

    [15.0][FIX] base_auction: remove is_auction_user from mixin model (#118)

--- base_auction/models/mail_activity_mixin.py
+++ base_auction/models/mail_activity_mixin.py
@@ -7,9 +7,6 @@ from odoo import fields, models
 class MailActivityMixin(models.AbstractModel):
     _inherit = "mail.activity.mixin"
 
-    is_auction_user = fields.Boolean(
-        compute="_compute_is_auction_user", inverse="_inverse_is_auction_user"
-    )
     # Ading base_auction.group_auction_user to prevent access errors.
     # TODO: Check if this change is really valid.
     activity_ids = fields.One2many(
