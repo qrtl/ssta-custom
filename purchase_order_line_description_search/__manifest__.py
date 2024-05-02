3f1ebf328d00d282c0a69640cdb6b16216edf5b4
Aungkokolin1997 <lin@quartile.co>
2024-05-02 16:55:05 +0700

    [ADD] purchase_order_line_description_search

---
+++ purchase_order_line_description_search/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Line Description Search",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+}
