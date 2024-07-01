4fc6afd176e371dd826d0840b4da6760d604d983
Aungkokolin1997 <lin@quartile.co>
2024-07-01 09:53:22 +0000

    adj

--- sale_order_import/wizards/sale_order_import.py
+++ sale_order_import/wizards/sale_order_import.py
@@ -287,21 +287,16 @@ class ImportSale(models.TransientModel):
         if product_id_value not in product_dict.keys():
             product_product = self.env["product.product"]
             product = product_product.search([("default_code", "=", product_id_value)])
-            if not product:
+            if not product or len(product) > 1:
                 error_vals["error_message"] = (
                     error_vals["error_message"]
                     + _("Product: ")
                     + product_id_value
-                    + _(" not found!")
-                    + "\n"
-                )
-                error_vals["error"] = True
-            elif len(product) > 1:
-                error_vals["error_message"] = (
-                    error_vals["error_message"]
-                    + _("Product with internal reference ")
-                    + product_id_value
-                    + _(" is found more than once!")
+                    + (
+                        _(" not found!\n")
+                        if not product
+                        else _(" is found more than once!\n")
+                    )
                     + "\n"
                 )
                 error_vals["error"] = True
