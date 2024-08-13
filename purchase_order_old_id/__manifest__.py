6fbc55cbc21737e1cfa987a7db83ae3a89d83db7
Aungkokolin1997 <lin@quartile.co>
2024-08-13 10:04:39 +0000

    [ADD] partner_old_id, purchase_order_old_id

---
+++ purchase_order_old_id/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Old ID",
+    "version": "15.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "category": "Purchase",
+    "license": "AGPL-3",
+    "depends": ["purchase"],
+    "installable": True,
+}
