55513400f7a485c2a95a044bc831949c3f8af8d7
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-09-04 13:55:32 +0700

    [4819][ADD] product_view_adjust (#140)
    * [ADD] product_view_adjust

---
+++ product_view_adjust/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product View Adjust",
+    "version": "15.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "category": "Product",
+    "license": "AGPL-3",
+    "depends": ["delivery"],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
