71fc2c77c50064ea07b0a23c395c493eac1e87de
kanda999 <kanda@quartile.co>
2024-09-25 05:31:41 +0000

    remove the wrong comment

--- product_yahoo_auction_sst/models/yahoo_product_state.py
+++ product_yahoo_auction_sst/models/yahoo_product_state.py
@@ -7,8 +7,6 @@ from odoo import fields, models
 class YahooProductState(models.Model):
     _name = "yahoo.product.state"
 
-    # We assume that there is just a single (internal) quant per product;
-    # the performance risk of `store=True` is minimal.
     name = fields.Char(
         string="Status",
         require=True,
