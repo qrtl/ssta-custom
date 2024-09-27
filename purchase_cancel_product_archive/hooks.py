8cbed3aa9ca90977d32a3e37b6ca6df25bb919f7
Aungkokolin1997 <lin@quartile.co>
2024-09-27 06:43:33 +0000

    adj

--- purchase_cancel_product_archive/hooks.py
+++ purchase_cancel_product_archive/hooks.py
@@ -8,5 +8,6 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     cancel_pos = env["purchase.order"].search([("state", "=", "cancel")])
     products = cancel_pos.mapped("order_line.product_id")
+    products = products.filtered(lambda x: x.qty_available <= 0.0)
     products.write({"active": False})
     products.mapped("product_tmpl_id").write({"active": False})
