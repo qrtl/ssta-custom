9294c759c63816aeb6de875ad866663f421a74a9
Aungkokolin1997 <lin@quartile.co>
2024-11-25 10:59:10 +0000

    adj

--- product_carousel_image_attachment/models/product_image.py
+++ product_carousel_image_attachment/models/product_image.py
@@ -1,18 +1,20 @@
 # Copyright 2024 Quartile
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class ProductImage(models.Model):
     _inherit = "product.image"
 
+    resize_done = fields.Boolean()
+
     @api.model
     def _cron_resize_product_image(self, limit):
-        images = self.sudo().search([], limit=limit)
+        images = self.sudo().search(
+            [("can_image_1024_be_zoomed", "=", True), ("resize_done", "=", False)],
+            limit=limit,
+        )
         for image in images:
             image.image_1920 = self.env["ir.attachment"]._resize_image(image.image_1920)
-        if len(images) == limit:
-            self.env.ref(
-                "product_carousel_image_attachment.resize_product_image"
-            )._trigger()
+            image.resize_done = True
